0546ca762e4d2c3b3db1218d215e24176f2a9531
zgarnirawen <rawenzgarni1@gmail.com>
2025-08-09 10:25:47 +0100

    new update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,8 @@
-// src/middleware.ts
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // Routes that can be accessed while signed out
   publicRoutes: ["/", "/api/json(.*)"],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ["/api/json(.*)"]
+  ignoredRoutes: ["/api/json(.*)"],
 });
 
 export const config = {
