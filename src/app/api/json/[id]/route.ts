98c3f15bb496dc762ab35c93bf62f422baa08e5b
zgarnirawen <rawenzgarni1@gmail.com>
2025-08-09 06:25:25 +0100

    new modification

--- src/app/api/json/[id]/route.ts
+++ src/app/api/json/[id]/route.ts
@@ -1,12 +1,11 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, { params }: {
-  params: {
-    id: string
-  }
-}) {
-  const { id } = params;
+export async function GET(
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+  const { id } = await context.params;
 
   try {
     const json = await prisma.jsonData.findUnique({
@@ -15,12 +14,20 @@ export async function GET(request: NextRequest, { params }: {
         id: true,
         name: true,
         content: true,
-        createdAt: true
-      }
+        createdAt: true,
+      },
     });
+
+    if (!json) {
+      return NextResponse.json({ error: "Data not found" }, { status: 404 });
+    }
+
     return NextResponse.json(json);
-  } catch(error) {
-    console.error('Error fetching JSON data:', error);
-    return NextResponse.json({ error: 'Error fetching JSON data'}, { status: 500 })
+  } catch (error) {
+    console.error("Error fetching JSON data:", error);
+    return NextResponse.json(
+      { error: "Error fetching JSON data" },
+      { status: 500 }
+    );
   }
 }
